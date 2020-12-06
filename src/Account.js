374aa09e2844609a4f2a09961a0b8357ae8b9b87
kristbjörg <kristbjorg_@hotmail.com>
2020-12-06 01:24:48 +0000

    added account and favourite pages

---
+++ src/Account.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Account() {
+  return <div>Þetta er Account síðan !! </div>;
+}
+
+export default Account;
---
+++ src/Favourite.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Favourite() {
+  return <div>Helloooo þetta er Favourite síðan !! !! </div>;
+}
+
+export default Favourite;
