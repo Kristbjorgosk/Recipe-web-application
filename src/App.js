d7ac26eb7755593703a2bdcb0479df5bca3d6162
kristbjörg <kristbjorg_@hotmail.com>
2020-12-06 19:42:12 +0000

    pulling

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 
-import FrontPage from "./FrontPage";
-import TipsPage from "./TipsPage";
-import RecipesPage from "./Component/RecipesPage";
+import FrontPage from "./Pages/FrontPage";
+import TipsPage from "./Pages/TipsPage";
+import RecipesPage from "./Pages/RecipesPage";
 import Header from "./Component/Header";
 import Navbar from "./Component/Navbar";
 
