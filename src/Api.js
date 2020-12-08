3e2e2372cd9c392c0f3a2de7f7470e8e084f98c9
kristbjörg <kristbjorg_@hotmail.com>
2020-12-08 15:58:00 +0000

    commit to main

--- src/Api.js
+++ src/Api.js
@@ -1,16 +1,14 @@
 // kristbjörg api
-// const apiKey = "2b4a3fe9cfef40f98088b1e8fa1eb21b";
+const apiKey = "2b4a3fe9cfef40f98088b1e8fa1eb21b";
 
 // fake email api, fyrir emergencies
-const apiKey = "fe9c6af3222046ee98ffbe2e539910d1";
+// const apiKey = "fe9c6af3222046ee98ffbe2e539910d1";
 
 // fake email api 2, fyrir severe emergencies
 // const apiKey = "0176027674304d0494879335a34d0f8a";
 
 // fake email api 3, fyrir tóds bad emergencies
 // const apiKey = "7e2fa4398373448ca75c6ee591ef989e";
-
-
 
 // kolla fact generator
 const fungeneratorsApiKey = "u96Qdi57HBRgQrJEzWUmwAeF";
