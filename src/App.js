a80b5ef2f5ebe612178d89bca92554c63a58b1c4
Kollaaj <kolbrunaj@gmail.com>
2020-12-08 14:56:10 +0000

    switch transference just because I felt like it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
-import "./Style/main.css";
+import { Switch, Route } from "react-router-dom";
+
+import FrontPage from "./Pages/FrontPage";
+import TipsPage from "./Pages/TipsPage";
+import FavouritesPage from "./Pages/FavouritesPage";
+import RecipesFilterPage from "./Pages/RecipesFilterPage";
+
 import Header from "./Component/Header";
 import Navbar from "./Component/Navbar";
+
+import "./Style/main.css";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Switch>
+        <Route exact path="/">
+          <FrontPage />
+        </Route>
+        <Route path="/Tipspage">
+          <TipsPage />
+        </Route>
+        <Route path="/FavouritesPage">
+          <FavouritesPage />
+        </Route>
+        <Route path="/RecipesFilterPage">
+          <RecipesFilterPage />
+        </Route>
+      </Switch>
 
       <Navbar />
     </>
